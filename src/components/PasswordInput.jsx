f03a7e804a9f87efe940fc857cd4010c1441bf23
Mateus Elias Vieira <mateusifg16@gmail.com>
2024-08-02 13:28:13 -0300

    add header

--- src/components/PasswordInput.jsx
+++ src/components/PasswordInput.jsx
@@ -7,39 +7,50 @@ const PasswordInput = (props) => {
     const [passConfirm, setPassConfirm] = useState('');
 
     const handleClick = async () => {
+        if (pass !== "" && passConfirm !== "") {
+            if (pass === passConfirm) {
+                if (props.token) {
+                    try {
+                        const response = await axios.post(
+                            "https://educaguard.up.railway.app/api/recover/new-password",
+                            {
+                                token: props.token,
+                                newpassword: pass
+                            },
+                            {
+                                headers: {
+                                    'Content-Type': 'application/json'
+                                }
+                            }
+                        );
 
-        if (pass === passConfirm) {
-            if (props.token) {
-                await axios.post("https://educaguard.up.railway.app/api/recover/new-password",
-                    JSON.stringify({
-                        token:props.token,
-                        newpassword:pass
-                    })
-                )
-                    .then((response) => {
-                        console.log("resposta " + response.data);
-                        if (response.status === 200) { // 200 é o código de status HTTP para sucesso
+                        if (response.status === 200) {
                             alert(response.data.message);
                         } else {
                             alert("Erro ao recuperar a conta!");
                         }
-                    })
-                    .catch((err) => {
-                        console.log = ("Erro " + err.message);
+                    } catch (err) {
+                        console.log("Erro " + err.message);
                         alert("Falha ao realizar recuperação de conta!");
-                    });
+                    }
+                } else {
+                    alert("A URL está quebrada!");
+                }
             } else {
-                alert("A URL está quebrada!");
+                alert("As senhas não são as mesmas!");
             }
         } else {
-            alert("As senhas não são as mesmas!");
+            alert("Preencha os campos!")
         }
     };
 
     return (
         <div>
-            <input className="form-control form-control-lg" type="password" placeholder="Nova senha" aria-label=".form-control-lg example" value={pass} onChange={(e) => setPass(e.target.value)} />
-            <input className="form-control form-control-lg" type="password" placeholder="Confirme a nova senha" aria-label=".form-control-lg example" value={passConfirm} onChange={(e) => setPassConfirm(e.target.value)} />
+            <input className="form-control form-control-lg" type="password" placeholder="Nova senha"
+                   aria-label=".form-control-lg example" value={pass} onChange={(e) => setPass(e.target.value)}/>
+            <input className="form-control form-control-lg" type="password" placeholder="Confirme a nova senha"
+                   aria-label=".form-control-lg example" value={passConfirm}
+                   onChange={(e) => setPassConfirm(e.target.value)}/>
             <button className="btn btn-primary" onClick={handleClick}>Recuperar</button>
         </div>
     );
